6eeec0a255f6d88c69a0c1f257f69d7ab1a73707
Abdul Azeem <azsidhu8@gmail.com>
2023-08-20 20:34:44 +0200

    run django bash script

---
+++ DjangoRestApi/run_django.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the OPENAI_API_KEY environment variable
+export OPENAI_API_KEY=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
+
+# Run the Django development server
+python3 manage.py runserver
